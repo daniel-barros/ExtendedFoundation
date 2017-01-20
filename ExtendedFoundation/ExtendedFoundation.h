6418d6ac92ec6a57f431e07dd2a2958cd07fab96
Daniel Barros <dani.barros@me.com>
2017-01-20 15:01:40 +0100

    Initial Commit

---
+++ ExtendedFoundation/ExtendedFoundation.h
@@ -0,0 +1,19 @@
+//
+//  ExtendedFoundation.h
+//  ExtendedFoundation
+//
+//  Created by Daniel Barros López on 1/20/17.
+//  Copyright © 2017 Daniel Barros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExtendedFoundation.
+FOUNDATION_EXPORT double ExtendedFoundationVersionNumber;
+
+//! Project version string for ExtendedFoundation.
+FOUNDATION_EXPORT const unsigned char ExtendedFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExtendedFoundation/PublicHeader.h>
+
+

3f5c92d8dd6f1ddf53644d43af40dad81037b5d1
Daniel Barros <dani.barros@me.com>
2017-09-23 19:03:51 +0200

    Optional<String> improvements for swift 4

--- Sources/Extensions/Optional.swift
+++ Sources/Extensions/Optional.swift
@@ -8,21 +8,10 @@
 
 import Foundation
 
-public protocol Emptiable {
-    var isEmpty: Bool { get }
-}
-
-extension String: Emptiable {}
-
-// Extension for optional strings that provides the `isNilOrEmpty` property.
-public extension Optional where Wrapped: Emptiable {
+// Extension for optional strings that declares the `isNilOrEmpty` property.
+public extension Optional where Wrapped == String {
     /// `true` if `self == nil` or `self?.isEmpty == true`, `false` otherwise.
-    ///
-    /// Available for optional strings. If you want it for other type make it conform to the `Emptiable` protocol.
     var isNilOrEmpty: Bool {
-        switch self {
-        case .none: return true
-        case .some(let string): return string.isEmpty
-        }
+        return self?.isEmpty ?? true
     }
 }
